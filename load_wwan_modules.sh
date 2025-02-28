fdeb30018b58dd69386b51938ef24302191671e7
Doan Duy <doanduy09@example.com>
2025-02-28 23:55:53 +0700

    Add support for Telit WWAN (VID: 1bc7)

--- load_wwan_modules.sh
+++ load_wwan_modules.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Detecting WWAN card..."
-WWAN_VID=$(lsusb | grep -oP "(2c7c|1199|12d1|2cb7)")
+WWAN_VID=$(lsusb | grep -oP "(2c7c|1199|12d1|2cb7|1bc7)")
 if [ -n "$WWAN_VID" ]; then
     case $WWAN_VID in
         "2c7c") modprobe qmi_wwan && modprobe cdc_mbim && echo "Loaded modules for Quectel WWAN";;
         "1199") modprobe qmi_wwan && echo "Loaded modules for Sierra Wireless WWAN";;
         "12d1") modprobe option && modprobe usbserial && echo "Loaded modules for Huawei WWAN";;
         "2cb7") modprobe qmi_wwan && modprobe cdc_mbim && echo "Loaded modules for Fibocom WWAN";;
+        "1bc7") modprobe qmi_wwan && modprobe cdc_mbim && echo "Loaded modules for Telit WWAN";;
         *) echo "Unknown WWAN card detected";;
     esac
 else
